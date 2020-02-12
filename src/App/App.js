2d05d79dcfe3a53eba9594102c66c705c9639f9d
dolev <dolevizacson@gmail.com>
2020-02-12 19:41:18 +0200

    add routes for project, blog post and error page

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { style, constants, ProtectedRoute } from '../env/utils/access';
+import {
+  style,
+  constants,
+  ProtectedRoute,
+  ErrorPage,
+} from '../env/utils/access';
 import { connect } from 'react-redux';
 import { actions } from '../env/utils/access';
 
@@ -10,13 +15,15 @@ import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 
 // page component
-import Home from '../pages/home/Home';
-import Projects from '../pages/projects/Projects';
-import Blog from '../pages/blog/Blog';
-import Skills from '../pages/skills/Skills';
-import Contact from '../pages/contact';
-import Admin from '../pages/admin/Admin';
-import Login from '../pages/login/Login';
+import Home from '../pages/Home/Home';
+import Projects from '../pages/Projects/Projects';
+import Project from '../pages/Project/Project';
+import Blog from '../pages/Blog/Blog';
+import BlogPost from '../pages/BlogPost/BlogPost';
+import Skills from '../pages/Skills/Skills';
+import Contact from '../pages/Contact/Contact';
+import Admin from '../pages/Admin/Admin';
+import Login from '../pages/Login/Login';
 
 // style
 import { AppStyle } from './AppStyle';
@@ -59,11 +66,22 @@ class App extends Component {
                     <Switch>
                       <Route path={homeRoute} exact component={Home} />
                       <Route path={projectsRoute} exact component={Projects} />
+                      <Route
+                        path={`${projectsRoute}/:id`}
+                        exact
+                        component={Project}
+                      />
                       <Route path={skillsRoute} exact component={Skills} />
                       <Route path={blogRoute} exact component={Blog} />
+                      <Route
+                        path={`${blogRoute}/:id`}
+                        exact
+                        component={BlogPost}
+                      />
                       <Route path={contactRoute} exact component={Contact} />
                       <ProtectedRoute path={adminRoute} component={Admin} />
                       <Route path={loginRoute} exact component={Login} />
+                      <Route component={ErrorPage} />
                     </Switch>
                   </Main>
                   <Footer />
@@ -81,7 +99,4 @@ const mapDispatchToProps = dispatch => ({
   isLoggedIn: () => dispatch(isLoggedIn()),
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(App);
+export default connect(null, mapDispatchToProps)(App);
