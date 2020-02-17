e167e0200a3e45619d346bb11ffb0b29d0c226f8
dolev <dolevizacson@gmail.com>
2020-02-17 19:05:42 +0200

    changes to api server axios settings

--- src/env/servers/portfolioApiServer.js
+++ src/env/servers/portfolioApiServer.js
@@ -5,11 +5,11 @@ import { constants } from '../utils/access';
 rax.attach();
 
 axios.defaults.baseURL = constants.baseURL;
-axios.defaults.timeout = 1000;
+axios.defaults.timeout = 2000;
 axios.defaults.withCredentials = true;
 axios.defaults.raxConfig = {
   noResponseRetries: 3,
-  retryDelay: 1000,
+  retryDelay: 500,
   httpMethodsToRetry: ['GET'],
 };
 
