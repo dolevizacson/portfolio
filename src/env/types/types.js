9a1f078da7e053c2a725c9006ad15a82300ff2f6
dolev <dolevizacson@gmail.com>
2020-02-17 19:16:01 +0200

    unite types to one object

--- src/env/types/types.js
+++ src/env/types/types.js
@@ -1,82 +1,130 @@
-export const skills = {
-  readAllRequest: 'READ_SKILLS_LIST_REQUEST',
-  readAllSuccess: 'READ_SKILLS_LIST_SUCCESS',
-  readAllFail: 'READ_SKILLS_LIST_FAILURE',
+export default {
+  init: {
+    isLoggedIn: 'IS_LOGGED_IN_INIT',
+  },
+  skills: {
+    readAll: 'READ_SKILLS_LIST',
+    readAllRequest: 'READ_SKILLS_LIST_REQUEST',
+    readAllSuccess: 'READ_SKILLS_LIST_SUCCESS',
+    readAllFail: 'READ_SKILLS_LIST_FAILURE',
 
-  createRequest: 'CREATE_SKILLS_LIST_REQUEST',
-  createSuccess: 'CREATE_SKILLS_LIST_SUCCESS',
-  createFail: 'CREATE_SKILLS_LIST_FAILURE',
+    createRequest: 'CREATE_SKILLS_LIST_REQUEST',
+    createSuccess: 'CREATE_SKILLS_LIST_SUCCESS',
+    createFail: 'CREATE_SKILLS_LIST_FAILURE',
 
-  updateRequest: 'UPDATE_SKILLS_LIST_REQUEST',
-  updateSuccess: 'UPDATE_SKILLS_LIST_SUCCESS',
-  updateFail: 'UPDATE_SKILLS_LIST_FAILURE',
+    updateRequest: 'UPDATE_SKILLS_LIST_REQUEST',
+    updateSuccess: 'UPDATE_SKILLS_LIST_SUCCESS',
+    updateFail: 'UPDATE_SKILLS_LIST_FAILURE',
 
-  deleteRequest: 'DELETE_SKILLS_LIST_REQUEST',
-  deleteSuccess: 'DELETE_SKILLS_LIST_SUCCESS',
-  deleteFail: 'DELETE_SKILLS_LIST_FAILURE',
-};
+    deleteRequest: 'DELETE_SKILLS_LIST_REQUEST',
+    deleteSuccess: 'DELETE_SKILLS_LIST_SUCCESS',
+    deleteFail: 'DELETE_SKILLS_LIST_FAILURE',
+  },
+  blog: {
+    readAllActive: 'READ_ACTIVE_BLOG_POSTS',
+    readAllActiveRequest: 'READ_ACTIVE_BLOG_POSTS_REQUEST',
+    readAllActiveSuccess: 'READ_ACTIVE_BLOG_POSTS_SUCCESS',
+    readAllActiveFail: 'READ_ACTIVE_BLOG_POSTS_FAILURE',
 
-export const blog = {
-  readAllActiveRequest: 'READ_ACTIVE_BLOG_POSTS_REQUEST',
-  readAllActiveSuccess: 'READ_ACTIVE_BLOG_POSTS_SUCCESS',
-  readAllActiveFail: 'READ_ACTIVE_BLOG_POSTS_FAILURE',
+    readAll: 'READ_BLOG_POSTS',
+    readAllRequest: 'READ_BLOG_POSTS_REQUEST',
+    readAllSuccess: 'READ_BLOG_POSTS_SUCCESS',
+    readAllFail: 'READ_BLOG_POSTS_FAILURE',
 
-  readAllRequest: 'READ_BLOG_POSTS_REQUEST',
-  readAllSuccess: 'READ_BLOG_POSTS_SUCCESS',
-  readAllFail: 'READ_BLOG_POSTS_FAILURE',
+    read: 'READ_BLOG_POST',
+    readRequest: 'READ_BLOG_POST_REQUEST',
+    readSuccess: 'READ_BLOG_POST_SUCCESS',
+    readFail: 'READ_BLOG_POST_FAILURE',
 
-  createRequest: 'CREATE_BLOG_POST_REQUEST',
-  createSuccess: 'CREATE_BLOG_POST_SUCCESS',
-  createFail: 'CREATE_BLOG_POST_FAILURE',
+    createRequest: 'CREATE_BLOG_POST_REQUEST',
+    createSuccess: 'CREATE_BLOG_POST_SUCCESS',
+    createFail: 'CREATE_BLOG_POST_FAILURE',
 
-  updateRequest: 'UPDATE_BLOG_POST_REQUEST',
-  updateSuccess: 'UPDATE_BLOG_POST_SUCCESS',
-  updateFail: 'UPDATE_BLOG_POST_FAILURE',
+    updateRequest: 'UPDATE_BLOG_POST_REQUEST',
+    updateSuccess: 'UPDATE_BLOG_POST_SUCCESS',
+    updateFail: 'UPDATE_BLOG_POST_FAILURE',
 
-  toggleRequest: 'TOGGLE_BLOG_POST_REQUEST',
-  toggleSuccess: 'TOGGLE_BLOG_POST_SUCCESS',
-  toggleFail: 'TOGGLE_BLOG_POST_FAILURE',
+    toggleRequest: 'TOGGLE_BLOG_POST_REQUEST',
+    toggleSuccess: 'TOGGLE_BLOG_POST_SUCCESS',
+    toggleFail: 'TOGGLE_BLOG_POST_FAILURE',
 
-  deleteRequest: 'DELETE_BLOG_POST_REQUEST',
-  deleteSuccess: 'DELETE_BLOG_POST_SUCCESS',
-  deleteFail: 'DELETE_BLOG_POST_FAILURE',
+    deleteRequest: 'DELETE_BLOG_POST_REQUEST',
+    deleteSuccess: 'DELETE_BLOG_POST_SUCCESS',
+    deleteFail: 'DELETE_BLOG_POST_FAILURE',
 
-  change: 'CHANGE_BLOG_POST',
-  reset: 'RESET_BLOG_POST',
-};
+    change: 'CHANGE_BLOG_POST',
+    reset: 'RESET_BLOG_POST',
+  },
+  taskList: {
+    readAll: 'READ_TASK_LIST',
+    readAllRequest: 'READ_TASK_LIST_REQUEST',
+    readAllSuccess: 'READ_TASK_LIST_SUCCESS',
+    readAllFail: 'READ_TASK_LIST_FAILURE',
 
-export const taskList = {
-  readAllRequest: 'READ_TASK_LIST_REQUEST',
-  readAllSuccess: 'READ_TASK_LIST_SUCCESS',
-  readAllFail: 'READ_TASK_LIST_FAILURE',
+    createRequest: 'CREATE_TASK_REQUEST',
+    createSuccess: 'CREATE_TASK_SUCCESS',
+    createFail: 'CREATE_TASK_FAILURE',
 
-  createRequest: 'CREATE_TASK_REQUEST',
-  createSuccess: 'CREATE_TASK_SUCCESS',
-  createFail: 'CREATE_TASK_FAILURE',
+    updateRequest: 'UPDATE_TASK_REQUEST',
+    updateSuccess: 'UPDATE_TASK_SUCCESS',
+    updateFail: 'UPDATE_TASK_FAILURE',
 
-  updateRequest: 'UPDATE_TASK_REQUEST',
-  updateSuccess: 'UPDATE_TASK_SUCCESS',
-  updateFail: 'UPDATE_TASK_FAILURE',
+    toggleRequest: 'TOGGLE_TASK_REQUEST',
+    toggleSuccess: 'TOGGLE_TASK_SUCCESS',
+    toggleFail: 'TOGGLE_TASK_FAILURE',
 
-  toggleRequest: 'TOGGLE_TASK_REQUEST',
-  toggleSuccess: 'TOGGLE_TASK_SUCCESS',
-  toggleFail: 'TOGGLE_TASK_FAILURE',
+    deleteRequest: 'DELETE_TASK_REQUEST',
+    deleteSuccess: 'DELETE_TASK_SUCCESS',
+    deleteFail: 'DELETE_TASK_FAILURE',
+  },
+  auth: {
+    loginRequest: 'LOGIN_REQUEST',
+    loginSuccess: 'LOGIN_SUCCESS',
+    loginFail: 'LOGIN_FAILURE',
 
-  deleteRequest: 'DELETE_TASK_REQUEST',
-  deleteSuccess: 'DELETE_TASK_SUCCESS',
-  deleteFail: 'DELETE_TASK_FAILURE',
-};
+    logoutRequest: 'LOGOUT_REQUEST',
+    logoutSuccess: 'LOGOUT_SUCCESS',
+    logoutFail: 'LOGOUT_FAILURE',
 
-export const auth = {
-  loginRequest: 'LOGIN_REQUEST',
-  loginSuccess: 'LOGIN_SUCCESS',
-  loginFail: 'LOGIN_FAILURE',
+    isLoggedInRequest: 'IS_LOGGED_IN_REQUEST',
+    isLoggedInSuccess: 'IS_LOGGED_IN_SUCCESS',
+    isLoggedInFail: 'IS_LOGGED_IN_FAILURE',
+  },
+  projects: {
+    readAllActive: 'READ_ACTIVE_PROJECTS',
+    readAllActiveRequest: 'READ_ACTIVE_PROJECTS_REQUEST',
+    readAllActiveSuccess: 'READ_ACTIVE_PROJECTS_SUCCESS',
+    readAllActiveFail: 'READ_ACTIVE_PROJECTS_FAILURE',
 
-  logoutRequest: 'LOGOUT_REQUEST',
-  logoutSuccess: 'LOGOUT_SUCCESS',
-  logoutFail: 'LOGOUT_FAILURE',
+    readAll: 'READ_PROJECTS',
+    readAllRequest: 'READ_PROJECTS_REQUEST',
+    readAllSuccess: 'READ_PROJECTS_SUCCESS',
+    readAllFail: 'READ_PROJECTS_FAILURE',
 
-  isLoggedInRequest: 'IS_LOGGED_IN_REQUEST',
-  isLoggedInSuccess: 'IS_LOGGED_IN_SUCCESS',
-  isLoggedInFail: 'IS_LOGGED_IN_FAILURE',
+    read: 'READ_PROJECT',
+    readRequest: 'READ_PROJECT_REQUEST',
+    readSuccess: 'READ_PROJECT_SUCCESS',
+    readFail: 'READ_PROJECT_FAILURE',
+
+    createRequest: 'CREATE_PROJECT_REQUEST',
+    createSuccess: 'CREATE_PROJECT_SUCCESS',
+    createFail: 'CREATE_PROJECT_FAILURE',
+
+    updateRequest: 'UPDATE_PROJECT_REQUEST',
+    updateSuccess: 'UPDATE_PROJECT_SUCCESS',
+    updateFail: 'UPDATE_PROJECT_FAILURE',
+
+    toggleRequest: 'TOGGLE_PROJECT_REQUEST',
+    toggleSuccess: 'TOGGLE_PROJECT_SUCCESS',
+    toggleFail: 'TOGGLE_PROJECT_FAILURE',
+
+    deleteRequest: 'DELETE_PROJECT_REQUEST',
+    deleteSuccess: 'DELETE_PROJECT_SUCCESS',
+    deleteFail: 'DELETE_PROJECT_FAILURE',
+  },
+  contact: {
+    createRequest: 'CREATE_MAIL_REQUEST',
+    createSuccess: 'CREATE_MAIL_SUCCESS',
+    createFail: 'CREATE_MAIL_FAILURE',
+  },
 };
