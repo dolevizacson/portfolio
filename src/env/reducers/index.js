d0ed95a307ec67dd60e91da8a8c46e20ea69058e
dolev <dolevizacson@gmail.com>
2020-02-17 18:55:13 +0200

    add blog post, projects list, project, and init reducers to main
    reducers file

--- src/env/reducers/index.js
+++ src/env/reducers/index.js
@@ -3,16 +3,22 @@ import { combineReducers } from 'redux';
 // reducers
 import { skillsListReducer } from './skillsListReducers';
 import { authReducer } from './authReducers';
-import { blogPostsReducer } from './blogPostsReducers';
+import { blogPostsReducer, blogPostReducer } from './blogPostsReducers';
 import { loadingReducer } from './loadingReducer';
 import { errorsReducer } from './errorsReducer';
 import { taskListreducer } from './taskListReducers';
+import { projectsReducer, projectReducer } from './projectsReducers';
+import { initReducer } from './initReducer';
 
 export default combineReducers({
   skillsList: skillsListReducer,
   blogPostsList: blogPostsReducer,
+  blogPost: blogPostReducer,
   isLoggedIn: authReducer,
   isLoading: loadingReducer,
   errors: errorsReducer,
   taskList: taskListreducer,
+  projectsList: projectsReducer,
+  project: projectReducer,
+  init: initReducer,
 });
