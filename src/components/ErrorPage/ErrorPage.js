05c80d430b0326b309bd350cf80f4d7a7992261b
dolev <dolevizacson@gmail.com>
2021-07-25 17:55:31 +0300

    minor changes

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import { UiTextBlock } from '../../env/utils/access';
 
 // style
-import {
-  ErrorPageStyle,
-  ErrorPageHeaderStyle,
-  ErrorPageMessageStyle,
-} from './ErrorPageStyle';
+const ErrorPageStyle = styled.section`
+  display: grid;
+  align-items: center;
+  min-height: 100%;
+`;
 
-const ErrorPage = ({ message, component }) => {
+const ErrorPageContainerStyle = styled(UiTextBlock)`
+  grid-auto-rows: min-content;
+`;
+const ErrorPageHeaderStyle = styled.h2`
+  text-align: center;
+  font-size: 15rem;
+`;
+const ErrorPageBodyStyle = styled.h3`
+  text-align: center;
+  color: var(--ui-font-color-1);
+  font-size: 2rem;
+`;
+
+const ErrorPage = (props) => {
+  const { message, component } = props;
   return (
     <ErrorPageStyle>
-      <ErrorPageHeaderStyle>404</ErrorPageHeaderStyle>
-      {message && <ErrorPageMessageStyle>{message}</ErrorPageMessageStyle>}
-      {component}
+      {component || (
+        <>
+          <ErrorPageContainerStyle
+            header={<ErrorPageHeaderStyle>404</ErrorPageHeaderStyle>}
+          >
+            <ErrorPageBodyStyle>
+              {message ||
+                "oh oh something went wrong you probably didn't meant to go here"}
+            </ErrorPageBodyStyle>
+          </ErrorPageContainerStyle>
+        </>
+      )}
     </ErrorPageStyle>
   );
 };
