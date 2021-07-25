793a2ac43560b50a5b376ab6686bb8d760b3fee8
dolev <dolevizacson@gmail.com>
2021-07-25 18:51:46 +0300

    changes to home page content

--- src/pages/home/components/About/staticContent.js
+++ src/pages/home/components/About/staticContent.js
@@ -1,6 +1,9 @@
-export default {
+const staticContent = {
+  header: `Dolev Izacson full-stack developer`,
   about: `Full Stack developer, biotechnology engineer,
-           love to learn and understand how things work.
-            Love to create and design my own projects`,
-  moto: `Try to find moments to laugh, even in bad times`,
+   love to learn new things and understand how things work.
+    Love to create and design my own projects`,
+  moto: `Try to find moments to laugh, even at hard times`,
 };
+
+export default staticContent;
