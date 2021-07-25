a73223f65be9f10b38acfc14d486da07c24765eb
dolev <dolevizacson@gmail.com>
2021-07-25 17:49:58 +0300

    add container in order to center content

--- src/App/components/Main/Main.js
+++ src/App/components/Main/Main.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // style
-import { MainStyle } from './MainStyle';
+const MainStyle = styled.main`
+  display: grid;
+  grid-template-columns:
+    minmax(1rem, 1fr) [main-start] minmax(min-content, 90rem)
+    [main-end] minmax(1rem, 1fr);
+  min-height: calc(100vh - Var(--navbar-height));
+`;
+
+const MainContainerStyle = styled.div`
+  grid-column: main-start/main-end;
+  margin: var(--ui-top-bottom-margin);
+`;
 
 const Main = ({ children }) => {
-  return <MainStyle>{children}</MainStyle>;
+  return (
+    <MainStyle>
+      <MainContainerStyle>{children}</MainContainerStyle>
+    </MainStyle>
+  );
 };
 
 export default Main;
