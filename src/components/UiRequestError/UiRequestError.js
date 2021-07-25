64df05fb8d2eebf92f4306639db40d7388ccee44
dolev <dolevizacson@gmail.com>
2021-07-25 18:01:49 +0300

    change to use text message component

--- src/components/UiRequestError/UiRequestError.js
+++ src/components/UiRequestError/UiRequestError.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import { UiButton, UiTextMessage } from '../../env/utils/access';
 
 // style
-import {
-  UiRequestErrorStyle,
-  UiRequestErrorMessageStyle,
-} from './UiRequestErrorStyle';
+const UiRequestErrorStyle = styled(UiTextMessage)``;
 
-const UiRequestError = ({ message, component }) => {
+const UiRequestErrorBodyStyle = styled.div`
+  display: grid;
+  grid-gap: var(--ui-grid-gap-2);
+  grid-template-rows: repeat(2, 1fr);
+
+  justify-items: center;
+
+  font-size: 1.8rem;
+  font-weight: 500;
+`;
+
+const UiRequestError = (props) => {
+  const { errorMessage, request, buttonText } = props;
+
   return (
     <UiRequestErrorStyle>
-      {message && (
-        <UiRequestErrorMessageStyle>{message}</UiRequestErrorMessageStyle>
-      )}
-      {component}
+      <UiRequestErrorBodyStyle>
+        {errorMessage || 'Something went wrong'}
+        {request && (
+          <UiButton onClick={request}>{buttonText || 'try to fix it'}</UiButton>
+        )}
+      </UiRequestErrorBodyStyle>
     </UiRequestErrorStyle>
   );
 };
