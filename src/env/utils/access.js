1559c0b2189c1b49fd3775952cbad4d7b3656c2f
dolev <dolevizacson@gmail.com>
2021-07-25 18:29:04 +0300

    add new components to access file

--- src/env/utils/access.js
+++ src/env/utils/access.js
@@ -2,23 +2,34 @@ import GlobalStyle from '../style/GlobalStyle';
 import MainTheme from '../style/MainTheme';
 
 export { default as types } from '../types/types';
-export { default as classes } from '../classes/classes';
-export { default as errors } from '../errors/errors';
-export { default as functions } from '../functions/functions';
-export { default as errorHandlers } from '../errorHandlers/errorHandlers';
+export * as classes from '../classes/classes';
+export * as errors from '../errors/errors';
+export * as functions from '../functions/functions';
+export * as errorHandlers from '../errorHandlers/errorHandlers';
 export { default as constants } from '../constants/constants';
 export { default as portfolioApiServer } from '../servers/portfolioApiServer';
-export { default as services } from '../services/services';
-export { default as actions } from '../actions/actions';
+export * as services from '../services/services';
+export * as actions from '../actions/actions';
 
 // global components
-export { default as UiHeader } from '../../components/UiHeader/UiHeader';
-export { default as UiLoading } from '../../components/UiLoading/UiLoading';
+
 export { default as ProtectedRoute } from '../../components/ProtectedRoute/ProtectedRoute';
-export { default as ErrorPage } from '../../components/ErrorPage/ErrorPage';
-export { default as UiRequstError } from '../../components/UiRequestError/UiRequestError';
 export { default as UiRequest } from '../../components/UiRequest/UiRequest';
-export { default as BackButton } from '../../components/BackButton/BackButton';
+export { default as UiHeader } from '../../components/UiHeader/UiHeader';
+export { default as BlogHeader } from '../../components/BlogHeader/BlogHeader';
+export { default as UiButtonsContainer } from '../../components/UiButtonsContainer/UiButtonsContainer';
+export { default as UiLoading } from '../../components/UiLoading/UiLoading';
+export { default as UiTextBlock } from '../../components/UiTextBlock/UiTextBlock';
+export { default as BlogTextBlock } from '../../components/BlogTextBlock/BlogTextBlock';
+export { default as UiTextMessage } from '../../components/UiTextMessage/UiTextMessage';
+export { default as UiRequestError } from '../../components/UiRequestError/UiRequestError';
+export { default as UiButton } from '../../components/UiButton/UiButton';
+export { default as UiBackButton } from '../../components/UiBackButton/UiBackButton';
+export { default as UiFormInputField } from '../../components/UiFormInputField/UiFormInputField';
+export { default as ErrorPage } from '../../components/ErrorPage/ErrorPage';
+export { default as UiImageContainer } from '../../components/UiImageContainer/UiImageContainer';
+export { default as AppModal } from '../../components/AppModal/AppModal';
+export { default as AppLogo } from '../../components/AppLogo/AppLogo';
 
 export const style = {
   GlobalStyle,
