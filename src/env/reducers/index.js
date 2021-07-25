04bba4c4e5eb225462c672e01e3fddce51d69c14
dolev <dolevizacson@gmail.com>
2021-07-25 18:17:37 +0300

    add new reducers

--- src/env/reducers/index.js
+++ src/env/reducers/index.js
@@ -1,24 +1,28 @@
 import { combineReducers } from 'redux';
 
 // reducers
-import { skillsListReducer } from './skillsListReducers';
+import { skillsListsReducer, skillsListReducer } from './skillsListReducers';
 import { authReducer } from './authReducers';
 import { blogPostsReducer, blogPostReducer } from './blogPostsReducers';
 import { loadingReducer } from './loadingReducer';
 import { errorsReducer } from './errorsReducer';
-import { taskListreducer } from './taskListReducers';
+import { taskListReducer, taskReducer } from './taskListReducers';
 import { projectsReducer, projectReducer } from './projectsReducers';
 import { initReducer } from './initReducer';
+import { contactReducer } from './contactReducers';
 
 export default combineReducers({
+  projectsList: projectsReducer,
+  project: projectReducer,
+  skillsLists: skillsListsReducer,
   skillsList: skillsListReducer,
+  taskList: taskListReducer,
+  task: taskReducer,
   blogPostsList: blogPostsReducer,
   blogPost: blogPostReducer,
+  messageSent: contactReducer,
   isLoggedIn: authReducer,
   isLoading: loadingReducer,
   errors: errorsReducer,
-  taskList: taskListreducer,
-  projectsList: projectsReducer,
-  project: projectReducer,
   init: initReducer,
 });
