27add3a617b1a340c9836909e20ee2b58b931e90
dolev <dolevizacson@gmail.com>
2021-07-25 18:17:22 +0300

    remove error counting, add reset errors case

--- src/env/reducers/errorsReducer.js
+++ src/env/reducers/errorsReducer.js
@@ -1,6 +1,14 @@
-export const errorsReducer = (state = { errorCount: 0 }, action) => {
+import { types } from '../utils/access';
+
+const { errors } = types;
+
+export const errorsReducer = (state = {}, action) => {
   const { type, payload } = action;
   const matches = /(.*)_(REQUEST|FAILURE)/.exec(type);
+
+  if (type === errors.resetErrors) {
+    return {};
+  }
 
   if (!matches) {
     return state;
@@ -10,10 +18,7 @@ export const errorsReducer = (state = { errorCount: 0 }, action) => {
 
   return {
     ...state,
-    errorCount:
-      state.errorCount +
-      (requestState === 'FAILURE' ? 1 : state.errorCount > 0 ? -1 : 0),
     [requestName]:
-      requestState === 'FAILURE' ? payload.message || 'Server Error' : '',
+      requestState === 'FAILURE' ? payload.message || 'Server Error' : null,
   };
 };
