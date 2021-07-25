75717bc530f9667de462385a1a8411180f055887
dolev <dolevizacson@gmail.com>
2021-07-25 18:20:09 +0300

    add reducers case for get lists, add a reducer for single item

--- src/env/reducers/skillsListReducers.js
+++ src/env/reducers/skillsListReducers.js
@@ -1,14 +1,33 @@
-import { types } from '../utils/access';
+import { types, constants } from '../utils/access';
+
+// constants
+const { skillsListInitialState } = constants;
 
 // types
 const { skills } = types;
 
-export const skillsListReducer = (state = [], action) => {
+export const skillsListsReducer = (state = [], action) => {
   switch (action.type) {
+    case skills.readAllActiveSuccess:
     case skills.readAllSuccess:
       return [...action.payload];
+    case skills.toggleSuccess:
+      return state.map((skillsList) =>
+        skillsList._id === action.payload._id ? action.payload : skillsList
+      );
     case skills.deleteSuccess:
-      return state.filter(skillsList => skillsList._id !== action.payload._id);
+      return state.filter(
+        (skillsList) => skillsList._id !== action.payload._id
+      );
+    default:
+      return state;
+  }
+};
+
+export const skillsListReducer = (state = skillsListInitialState, action) => {
+  switch (action.type) {
+    case skills.readSuccess:
+      return { ...action.payload };
     default:
       return state;
   }
--- src/env/reducers/taskListReducers.js
+++ src/env/reducers/taskListReducers.js
@@ -1,18 +1,32 @@
-import { types } from '../utils/access';
+import { types, constants } from '../utils/access';
+
+//constants
+const { taskInitialState } = constants;
 
 // types
 const { taskList } = types;
 
-export const taskListreducer = (state = [], action) => {
+export const taskListReducer = (state = [], action) => {
   switch (action.type) {
     case taskList.readAllSuccess:
+    case taskList.readAllActiveSuccess:
       return [...action.payload];
+    case taskList.updateSuccess:
     case taskList.toggleSuccess:
-      return state.map(blogPost => {
-        return blogPost._id === action.payload._id ? action.payload : blogPost;
+      return state.map((task) => {
+        return task._id === action.payload._id ? action.payload : task;
       });
     case taskList.deleteSuccess:
-      return state.filter(blogPost => blogPost._id !== action.payload._id);
+      return state.filter((blogPost) => blogPost._id !== action.payload._id);
+    default:
+      return state;
+  }
+};
+
+export const taskReducer = (state = taskInitialState, action) => {
+  switch (action.type) {
+    case taskList.readSuccess:
+      return { ...action.payload };
     default:
       return state;
   }
