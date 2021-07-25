ff61baf1c4317ead399ccd17e97f521c06080fe4
dolev <dolevizacson@gmail.com>
2021-07-25 17:32:39 +0300

    move routes to new component

--- src/App/App.js
+++ src/App/App.js
@@ -1,101 +1,54 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import {
-  style,
-  constants,
-  ProtectedRoute,
-  ErrorPage,
-} from '../env/utils/access';
+import styled from 'styled-components';
+import { style } from '../env/utils/access';
 import { connect } from 'react-redux';
 import { actions } from '../env/utils/access';
 
 // components
 import AppNavbar from './components/AppNavbar/AppNavbar';
 import Main from './components/Main/Main';
+import Routes from './components/Routes/Routes';
 import Footer from './components/Footer/Footer';
 
-// page component
-import Home from '../pages/Home/Home';
-import Projects from '../pages/Projects/Projects';
-import Project from '../pages/Project/Project';
-import Blog from '../pages/Blog/Blog';
-import BlogPost from '../pages/BlogPost/BlogPost';
-import Skills from '../pages/Skills/Skills';
-import Contact from '../pages/Contact/Contact';
-import Admin from '../pages/Admin/Admin';
-import Login from '../pages/Login/Login';
-
 // style
-import { AppStyle } from './AppStyle';
+const AppStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+`;
 
 // actions
 const { isLoggedIn } = actions.authActions;
-
-// constants
-const {
-  homeRoute,
-  projectsRoute,
-  skillsRoute,
-  blogRoute,
-  contactRoute,
-  adminRoute,
-  loginRoute,
-} = constants;
 
 // global style
 const { MainTheme } = style;
 const { GlobalStyle } = style;
 
 class App extends Component {
-  componentDidMount = () => {
+  componentDidMount = async () => {
     const { isLoggedIn } = this.props;
-    isLoggedIn();
+    await isLoggedIn();
   };
 
   render() {
     return (
       <>
         <MainTheme>
-          <>
-            <GlobalStyle />
-            <AppStyle>
-              <BrowserRouter>
-                <>
-                  <AppNavbar />
-                  <Main>
-                    <Switch>
-                      <Route path={homeRoute} exact component={Home} />
-                      <Route path={projectsRoute} exact component={Projects} />
-                      <Route
-                        path={`${projectsRoute}/:id`}
-                        exact
-                        component={Project}
-                      />
-                      <Route path={skillsRoute} exact component={Skills} />
-                      <Route path={blogRoute} exact component={Blog} />
-                      <Route
-                        path={`${blogRoute}/:id`}
-                        exact
-                        component={BlogPost}
-                      />
-                      <Route path={contactRoute} exact component={Contact} />
-                      <ProtectedRoute path={adminRoute} component={Admin} />
-                      <Route path={loginRoute} exact component={Login} />
-                      <Route component={ErrorPage} />
-                    </Switch>
-                  </Main>
-                  <Footer />
-                </>
-              </BrowserRouter>
-            </AppStyle>
-          </>
+          <GlobalStyle />
+          <AppStyle>
+            <AppNavbar />
+            <Main>
+              <Routes />
+            </Main>
+            <Footer />
+          </AppStyle>
         </MainTheme>
       </>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   isLoggedIn: () => dispatch(isLoggedIn()),
 });
 
