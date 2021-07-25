dc63aa7ab37f6abcd1090dd41dbc299c39a65cfa
dolev <dolevizacson@gmail.com>
2021-07-25 18:30:13 +0300

    add browser router component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
+// components
 import App from './App/App';
 
 ReactDOM.render(
-  <Provider store={store()}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
