8acb2630e66c7a4dfb0256609952e819ee0eab42
dolev <dolevizacson@gmail.com>
2021-07-25 18:27:31 +0300

    add mixins files

---
+++ src/env/style/Mixins.js
@@ -0,0 +1,19 @@
+import { css } from 'styled-components';
+
+export const navbarSeparator = () => css`
+  border-bottom: none;
+  box-shadow: var(--ui-shadow-1);
+`;
+
+export const centerContent = () => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const formModalStyle = () => css`
+  text-align: center;
+
+  font-size: 1.8rem;
+  font-weight: 500;
+`;
